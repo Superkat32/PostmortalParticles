46fc8e9c24d8fe2c19ed56fd45fa6a254a59db37
Superkat32 <superkat1806@gmail.com>
2023-03-20 21:38:14 -0400

    Started work on particles
    FEAT: Started work on the first particle, the portal
    Signed-off-by: Superkat32 <superkat1806@gmail.com>

---
+++ src/main/java/net/superkat/PostmortalClient.java
@@ -0,0 +1,13 @@
+package net.superkat;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.minecraft.client.particle.EndRodParticle;
+import net.minecraft.client.particle.FlameParticle;
+
+public class PostmortalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ParticleFactoryRegistry.getInstance().register(PostmortalMain.PORTAL, EndRodParticle.Factory::new);
+    }
+}
--- src/main/java/net/superkat/PostmortalMain.java
+++ src/main/java/net/superkat/PostmortalMain.java
@@ -2,6 +2,11 @@ package net.superkat;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +14,13 @@ public class PostmortalMain implements ModInitializer {
 	public static final String MOD_ID = "postmortalparticles";
 	public static final Logger LOGGER = LoggerFactory.getLogger("postmortalparticles");
 
+	public static final DefaultParticleType PORTAL = FabricParticleTypes.simple();
+
 	@Override
 	public void onInitialize() {
 
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier(MOD_ID, "portal"), PORTAL);
 		LOGGER.info("Hello Fabric world!");
+
 	}
 }
