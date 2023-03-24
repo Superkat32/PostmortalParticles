c6770a299b50c2c54907386e99e4195a66c3036b
Superkat32 <superkat1806@gmail.com>
2023-03-23 23:40:07 -0400

    Removed a bunch of commented out code
    REFACTOR: Removed a lot of commented out code
    Signed-off-by: Superkat32 <superkat1806@gmail.com>

--- src/main/java/net/superkat/PostmortalConfig.java
+++ src/main/java/net/superkat/PostmortalConfig.java
@@ -18,53 +18,6 @@ public class PostmortalConfig {
 
     @ConfigEntry public boolean myBoolean = true;
 
-//    public static void save() { /* save your config! */ }
-
-//    public Screen createGui(Screen parent) {
-//        YetAnotherConfigLib.createBuilder()
-//                .title(Text.of("Mod Name"))
-//                .category(ConfigCategory.createBuilder()
-//                        .name(Text.of("My Category"))
-//                        .tooltip(Text.of("This displays when you hover over a category button")) // optional
-//                        .option(Option.createBuilder(boolean.class)
-//                                .name(Text.of("My Boolean Option"))
-//                                .tooltip(Text.of("This option displays the basic capabilities of YetAnotherConfigLib")) // optional
-//                                .binding(
-//                                        true, // default
-//                                        () -> this.booleanToggle, // getter
-//                                        newValue -> this.booleanToggle = newValue // setter
-//                                )
-//                                .controller(BooleanController::new)
-//                                .build())
-//                        .build())
-//                .save(GuiTest::save)
-//                .build()
-//                .generateScreen(parent);
-//    }
-
-//    public static Screen getModConfigScreenFactory(Screen parent) {
-//        return YetAnotherConfigLib.create(ExampleConfig.INSTANCE, (defaults, config, builder) -> builder
-//                        .title(Text.literal("Wiki"))
-//                        .category(ConfigCategory.createBuilder()
-////                                .name(Text.literal("Suites"))
-////                                .option(ButtonOption.createBuilder()
-////                                        .name(Text.literal("Full Test Suite"))
-////                                        .controller(ActionController::new)
-//////                                        .action((screen, opt) -> Minecraft.getInstance().setScreen(getFullTestSuite(screen)))
-////                                        .build())
-////                                .group(OptionGroup.createBuilder()
-//                                    .name(Text.literal("Wiki"))
-//                                    .option(ButtonOption.createBuilder()
-//                                            .name(Text.literal("Get Started"))
-//                                            .controller(ActionController::new)
-//                                            .action((screen, opt) -> MinecraftClient.getInstance().setScreen(getWikiStarted((screen))))
-////                                                .action((yaclScreen, buttonOption) -> MinecraftClient.getInstance().setScreen(getWikiStarted(yaclScreen)))
-//                                            .build())
-////                                        .build())
-//                                .build())
-//                )
-//                .generateScreen(parent);
-//    }
     public static Screen makeScreen(Screen parent) {
         return YetAnotherConfigLib.create(INSTANCE, (defaults, config, builder) -> {
             var categoryBuilder = ConfigCategory.createBuilder()
@@ -90,28 +43,4 @@ public class PostmortalConfig {
                     .category(categoryBuilder.build());
         }).generateScreen(parent);
     }
-
-
-//    private static boolean myBooleanOption = true;
-//    private static Screen getWikiStarted(Screen parent) {
-//        return YetAnotherConfigLib.createBuilder()
-//                .title(Text.literal("Title"))
-//                .category(ConfigCategory.createBuilder()
-//                        .name(Text.literal("category"))
-//                        .tooltip(Text.literal("tooltip"))
-//                        .group(OptionGroup.createBuilder()
-//                                .name(Text.literal("group"))
-//                                .tooltip(Text.literal("group tooltip"))
-//                                .option(Option.createBuilder(boolean.class)
-//                                        .name(Text.literal("Boolean Option"))
-//                                        .tooltip(Text.literal("Boolean tooltip"))
-//                                        .binding()
-//                                        .controller(TickBoxController::new)
-//                                        .build()
-//                                ).build()
-//                        ).build()
-//                ).build()
-//                .generateScreen(parent);
-//    }
-
 }
