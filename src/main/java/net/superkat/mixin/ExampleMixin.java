f79bc2f13c565e8d4f6a65c35606d56c384c4858
Superkat32 <superkat1806@gmail.com>
2023-03-24 22:38:17 -0400

    Changed @Inject to @Redirect
    REFACTOR: Changed the @Inject to @Redirect in the ClientPlayNetworkHandler mixin. This is to remove the default particles from the totem of undying
    FEAT: Readded said removed default particles by adding it to the mixin file. This is to allow the config to change if the default particles are to be shown or not
    Signed-off-by: Superkat32 <superkat1806@gmail.com>

--- src/main/java/net/superkat/mixin/ExampleMixin.java
+++ src/main/java/net/superkat/mixin/ExampleMixin.java
@@ -2,16 +2,17 @@ package net.superkat.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.listener.TickablePacketListener;
-import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.superkat.PostmortalMain;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ExampleMixin implements TickablePacketListener, ClientPlayPacketListener {
@@ -24,13 +25,14 @@ public abstract class ExampleMixin implements TickablePacketListener, ClientPlay
 		this.client = client;
 	}
 
-	@Inject(method = "onEntityStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addEmitter(Lnet/minecraft/entity/Entity;Lnet/minecraft/particle/ParticleEffect;I)V"))
-	private void init(EntityStatusS2CPacket packet, CallbackInfo ci) {
+	@Redirect(method = "onEntityStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addEmitter(Lnet/minecraft/entity/Entity;Lnet/minecraft/particle/ParticleEffect;I)V"))
+	private void init(ParticleManager instance, Entity entity, ParticleEffect parameters, int maxAge) {
 //		Entity entity = packet.getEntity(this.world);
-		Entity entity = packet.getEntity(this.world);
+//		Entity entity = packet.getEntity(this.world);
 		this.world.addParticle(PostmortalMain.VORTEX, entity.getX(), entity.getY(), entity.getZ(), 0, 0, 0);
 		this.client.particleManager.addEmitter(entity, PostmortalMain.SPARKLE, 100);
 		this.client.particleManager.addEmitter(entity, PostmortalMain.EXPLOSION, 10);
+		this.client.particleManager.addEmitter(entity, ParticleTypes.TOTEM_OF_UNDYING, 30);
 //		this.client.particleManager.addParticle(PostmortalMain.PORTAL, entity.getX(), );
 //		this.client.particleManager.addEmitter(entity, ParticleTypes.END_ROD, 30);
 	}
