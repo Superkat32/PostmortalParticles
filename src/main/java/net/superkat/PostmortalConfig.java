1c8cc11cd993795f8168ca08b3449c9eb4b0e526
Superkat32 <superkat1806@gmail.com>
2023-03-24 23:10:10 -0400

    Added sparkles to the config
    FEAT: New config option for the sparkle particles(still doesn't do anything right now, though)
    Signed-off-by: Superkat32 <superkat1806@gmail.com>

--- src/main/java/net/superkat/PostmortalConfig.java
+++ src/main/java/net/superkat/PostmortalConfig.java
@@ -17,13 +17,17 @@ public class PostmortalConfig {
     public static final ConfigInstance<PostmortalConfig> INSTANCE = new GsonConfigInstance<>(PostmortalConfig.class, Path.of("./config/postmortal-config.json"));
 
     @ConfigEntry public boolean vortexParticle = true;
+    @ConfigEntry public boolean sparkleParticle = true;
 
 
     //TODO - Change all "Text.literal('string')" to work with the language file(e.g "Text.translatable('bleh.bleh.bleh")
     public static Screen makeScreen(Screen parent) {
         return YetAnotherConfigLib.create(INSTANCE, (defaults, config, builder) -> {
+            //Particles category
             var particlesCategoryBuilder = ConfigCategory.createBuilder()
                     .name(Text.translatable("postmortalparticles.category.particle"));
+
+            //Vortex particle group
             var vortexGroup = OptionGroup.createBuilder()
                     .name(Text.translatable("postmortalparticles.vortex.group"))
                     .tooltip(Text.translatable("postmortalparticles.vortex.group.tooltip"));
@@ -39,6 +43,24 @@ public class PostmortalConfig {
                     .build();
             vortexGroup.option(vortex);
             particlesCategoryBuilder.group(vortexGroup.build());
+
+            //Sparkle particle group
+            var sparkleGroup = OptionGroup.createBuilder()
+                    .name(Text.translatable("postmortalparticles.sparkle.group"))
+                    .tooltip(Text.translatable("postmortalparticles.sparkle.group.tooltip"));
+            var sparkle = Option.createBuilder(Boolean.class)
+                    .name(Text.translatable("postmortalparticles.sparkle.enabled"))
+                    .tooltip(Text.translatable("postmortalparticles.sparkle.enabled.tooltip"))
+                    .binding(
+                            defaults.sparkleParticle,
+                            () -> config.sparkleParticle,
+                            val -> config.sparkleParticle = val
+                    )
+                    .controller(booleanOption -> new BooleanController(booleanOption, true))
+                    .build();
+            sparkleGroup.option(sparkle);
+            particlesCategoryBuilder.group(sparkleGroup.build());
+
 
             return builder
                     .title(Text.translatable("postmortalparticles.title"))
