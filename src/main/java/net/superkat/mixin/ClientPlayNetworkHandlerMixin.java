e83a04a59bf07935bf1e3d9cb1910ac97984836c
Superkat32 <superkat1806@gmail.com>
2023-04-08 15:22:27 -0400

    Bump version
    BUILD: Bumped version
    REFACTOR: Attempted to fix multiplayer support
    Signed-off-by: Superkat32 <superkat1806@gmail.com>

--- src/main/java/net/superkat/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/net/superkat/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,9 +1,7 @@
 package net.superkat.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.listener.TickablePacketListener;
@@ -19,58 +17,61 @@ import static net.superkat.PostmortalMain.LOGGER;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ClientPlayNetworkHandlerMixin implements TickablePacketListener, ClientPlayPacketListener {
-	private final ClientWorld world;
-	private final MinecraftClient client;
-	public ClientPlayNetworkHandlerMixin(ClientWorld world, MinecraftClient client) {
-		super();
-		this.world = world;
-		this.client = client;
-	}
+//	private final ClientWorld world;
+//	private final MinecraftClient client;
+//	public ClientPlayNetworkHandlerMixin(ClientWorld world, MinecraftClient client) {
+//		super();
+//		this.world = world;
+//		this.client = client;
+//	}
 
+//	@Redirect(method = "onEntityStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addEmitter(Lnet/minecraft/entity/Entity;Lnet/minecraft/particle/ParticleEffect;I)V"))
+	//TODO - Check if using "instance.addParticle" is better than "world.addParticle"
 	@Redirect(method = "onEntityStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addEmitter(Lnet/minecraft/entity/Entity;Lnet/minecraft/particle/ParticleEffect;I)V"))
-	private void init(ParticleManager instance, Entity entity, ParticleEffect parameters, int maxAge) {
+	public void onEntityStatus(ParticleManager instance, Entity entity, ParticleEffect parameters, int maxAge) {
 		if(INSTANCE.getConfig().modEnabled) {
+//			World world = entity.getWorld();
 			if(INSTANCE.getConfig().spamLogs) {
 				LOGGER.info("Showing particles!");
 				logConfig(entity);
 			}
 			if(INSTANCE.getConfig().vortexParticle) {
-				this.world.addParticle(PostmortalMain.VORTEX, entity.getX(), entity.getBodyY(1.0), entity.getZ(), 0, 0, 0);
+				instance.addParticle(PostmortalMain.VORTEX, entity.getX(), entity.getBodyY(1.0), entity.getZ(), 0, 0, 0);
 			}
 			if(INSTANCE.getConfig().sparkleParticle) {
 				//Multiply the number by 20, to change the time from seconds to Minecraft ticks
 				float timer = INSTANCE.getConfig().sparkleTimer * 20;
-				this.client.particleManager.addEmitter(entity, PostmortalMain.SPARKLE, (int) timer);
+				instance.addEmitter(entity, PostmortalMain.SPARKLE, (int) timer);
 			}
 			if(INSTANCE.getConfig().sparkleExplosionParticle) {
-				this.client.particleManager.addEmitter(entity, PostmortalMain.EXPLOSION, 10);
+				instance.addEmitter(entity, PostmortalMain.EXPLOSION, 10);
 			}
 			if(INSTANCE.getConfig().totemParticle) {
-				this.world.addParticle(PostmortalMain.TOTEM, entity.getX(), entity.getBodyY(1.0), entity.getZ(), 0, 0, 0);
+				instance.addParticle(PostmortalMain.TOTEM, entity.getX(), entity.getBodyY(1.0), entity.getZ(), 0, 0, 0);
 			}
 			if(INSTANCE.getConfig().shatteredParticle) {
 				for(int amount = INSTANCE.getConfig().shatteredAmount; amount >= 1; amount--) {
-					this.world.addParticle(PostmortalMain.SHATTERED, entity.getX(), entity.getBodyY(1.0), entity.getZ(), 0, 0, 0);
+					instance.addParticle(PostmortalMain.SHATTERED, entity.getX(), entity.getBodyY(1.0), entity.getZ(), 0, 0, 0);
 				}
 			}
 			if(INSTANCE.getConfig().defaultParticles) {
 				float timer = INSTANCE.getConfig().defaultTimer * 20;
-				this.client.particleManager.addEmitter(entity, ParticleTypes.TOTEM_OF_UNDYING, (int) timer);
+				instance.addEmitter(entity, ParticleTypes.TOTEM_OF_UNDYING, (int) timer);
 			}
 			if(INSTANCE.getConfig().beamParticle) {
 				float timer = INSTANCE.getConfig().beamTime * 20;
-				this.client.particleManager.addEmitter(entity, PostmortalMain.BEAM, (int) timer);
+				instance.addEmitter(entity, PostmortalMain.BEAM, (int) timer);
 			}
 			if(INSTANCE.getConfig().trailParticle) {
 				float timer = INSTANCE.getConfig().trailTime * 20;
-				this.client.particleManager.addEmitter(entity, PostmortalMain.TRAIL, (int) timer);
+				instance.addEmitter(entity, PostmortalMain.TRAIL, (int) timer);
 			}
 		} else {
 			if(INSTANCE.getConfig().spamLogs) {
 				LOGGER.info("Not showing any particles beyond original");
 				LOGGER.info("Mod enabled/disabled status: " + INSTANCE.getConfig().modEnabled);
 			}
-			this.client.particleManager.addEmitter(entity, ParticleTypes.TOTEM_OF_UNDYING, 30);
+			instance.addEmitter(entity, ParticleTypes.TOTEM_OF_UNDYING, 30);
 		}
 	}
 
